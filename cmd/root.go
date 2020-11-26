455b54707f4e2f50f1a77414dbc23b35ebdbcd25
rick <rick@jenkins-zh.cn>
2020-11-26 21:45:37 +0800

    Fix the config not reading errors

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	appCmd "github.com/jenkins-zh/jenkins-cli/app/cmd"
 	cmdApp "github.com/jenkins-zh/jenkins-cli/app/cmd"
 	"github.com/jenkins-zh/jenkins-cli/app/cmd/common"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // Check do the health check of casc cmd
@@ -17,10 +19,17 @@ func (o *CASCOptions) Check() (err error) {
 
 func NewCasCCMD() (cascCmd *cobra.Command) {
 	opt := CASCOptions{}
+	rootCmd := appCmd.GetRootCommand()
 	cascCmd = &cobra.Command{
-		Use:   "jcli casc",
-		Short: "Configuration as Code",
-		Long:  "Configuration as Code",
+		Use:               "casc",
+		Short:             "Configuration as Code",
+		Long:              "Configuration as Code",
+		ValidArgsFunction: common.NoFileCompletion,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SetOut(os.Stdout)
+			return rootCmd.PersistentPreRunE(cmd, args)
+		},
+		PersistentPostRun: rootCmd.PersistentPostRun,
 	}
 	cmdApp.GetHealthCheckRegister().Register("jcli.casc.*", &opt)
 
